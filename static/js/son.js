1ccd780a97fc13b316c4b35d76536577d66d1e83
GustavoPMarcena <gustavopmarcena@outlook.com>
2023-07-16 15:20:49 -0300

    Finalização do projeto

--- static/js/son.js
+++ static/js/son.js
@@ -6,20 +6,20 @@ var navson = document.getElementsByClassName("son")[0]
 
 function setcond () {
     
-    if (navson.textContent == "Son") {
+    if (navson.textContent == "Som") {
         navson.innerText = "Off"
         navson.classList.remove('bi-volume-down-fill')
         navson.classList.add('bi-volume-mute-fill')
         
     } else {
-        navson.innerText = "Son"
+        navson.innerText = "Som"
         navson.classList.remove('bi-volume-mute-fill')
         navson.classList.add('bi-volume-down-fill')   
     }
 }
 
 function son () {
-    if (navson.textContent == "Son") {
+    if (navson.textContent == "Som") {
         var audio = document.getElementsByTagName("audio")[0]
         audio.play() 
     } 
